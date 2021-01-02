ac96193a1444a3e24ddccbd4253efdaff40d7463
joelmss93 <joelmss93@gmail.com>
2021-01-02 20:01:14 -0300

    Emails in dev mode

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
-import SessionsController from '../controllers/SessionsController';
 import ResetPasswordController from '../controllers/ResetPasswordController';
 
 const passwordRouter = Router();
