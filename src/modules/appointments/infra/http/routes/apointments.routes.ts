90765238c6b697f6cafcf726b798c0da14bdfdf8
joelmss93 <joelmss93@gmail.com>
2021-01-07 22:01:43 -0300

    Providers list

--- src/modules/appointments/infra/http/routes/apointments.routes.ts
+++ src/modules/appointments/infra/http/routes/apointments.routes.ts
@@ -9,13 +9,6 @@ const appointmentsController = new AppointmentsController();
 
 appointmentRouter.use(ensureAuthenticated);
 
-// appointmentRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-//   console.log(request.user);
-
-//   return response.json(appointments);
-// });
-
 appointmentRouter.post('/', appointmentsController.create);
 
 export default appointmentRouter;
