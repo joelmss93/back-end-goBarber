4f8393d53f47471098e794ad2af905536ab5c3f0
joelmss93 <joelmss93@gmail.com>
2021-01-16 17:43:16 -0300

    ambient variables

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'dc2b5dfa042dc15d08b16b1adc90fdae',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
